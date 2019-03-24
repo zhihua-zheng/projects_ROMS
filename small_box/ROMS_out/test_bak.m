1e5dad28bb009b2de5b63460afa07df8184f92de
zhihua-zheng <greengo@outlook.com>
2019-03-23 19:21:43 -0700

    Add velocity analysis section in 'test_bak.m'

--- small_box/ROMS_out/test_bak.m
+++ small_box/ROMS_out/test_bak.m
@@ -1,41 +1,69 @@
 
+
+%% General
+
 clear
 clc
-proj_root = '~/Documents/GitHub/Rutgers_ROMS/projects_ROMS/small_box';
+
+if ismac
+    proj_root = '/Volumes/projects_ROMS/small_box';
+elseif isunix
+    proj_root = '~/Documents/GitHub/Rutgers_ROMS/projects_ROMS/small_box';
+% elseif ispc
+    % Code to run on Windows platform
+else
+    disp('Platform not supported')
+end
+
+%HISname = [proj_root,'/ROMS_out/Inertial/roms_his.nc'];
+HISname = [proj_root,'/ROMS_out/TransWind/roms_his.nc'];
 
 %%
 
-time   = ncread('roms_his.nc','ocean_time');
-dstart = ncread('roms_his.nc','dstart'); % days since t_ref
+time   = ncread(HISname,'ocean_time');
+dt     = ncread(HISname,'dt');
+nHIS   = ncread(HISname,'nHIS');
+dstart = ncread(HISname,'dstart'); % days since t_ref
 
-zeta    = ncread('roms_his.nc','zeta');
-ubar    = ncread('roms_his.nc','ubar');
-vbar    = ncread('roms_his.nc','vbar');
-u       = ncread('roms_his.nc','u');
-v       = ncread('roms_his.nc','v');
-temp    = ncread('roms_his.nc','temp');
-salt    = ncread('roms_his.nc','salt');
+sustr = ncread(HISname,'sustr'); 
+svstr = ncread(HISname,'svstr');
+
+zeta    = ncread(HISname,'zeta');
+ubar    = ncread(HISname,'ubar');
+vbar    = ncread(HISname,'vbar');
+u       = ncread(HISname,'u');
+v       = ncread(HISname,'v');
+temp    = ncread(HISname,'temp');
+salt    = ncread(HISname,'salt');
 
 % background vertical mixing coefficient for tracers (temp & salt)
-Akt_bak = ncread('roms_his.nc','Akt_bak');
-AKs     = ncread('roms_his.nc','AKs'); % vertical mxing coefficient for salt
-AKt     = ncread('roms_his.nc','AKt'); % vertical mxing coefficient for temp
-AKv     = ncread('roms_his.nc','AKv'); % vertical mxing coefficient for momentum
-tke     = ncread('roms_his.nc','tke');
-gls     = ncread('roms_his.nc','gls'); % turbulent generic length scale
+Akt_bak = ncread(HISname,'Akt_bak');
+AKs     = ncread(HISname,'AKs'); % vertical mxing coefficient for salt
+AKt     = ncread(HISname,'AKt'); % vertical mxing coefficient for temp
+AKv     = ncread(HISname,'AKv'); % vertical mxing coefficient for momentum
+tke     = ncread(HISname,'tke');
+gls     = ncread(HISname,'gls'); % turbulent generic length scale
 
 t_ref = datenum('0001-01-01 00:00:00','yyyy-mm-dd HH:MM:SS');
 time  = time/3600/24 + t_ref;
 
-%%
 
-Vtransform  = ncread('roms_his.nc','Vtransform');
-Vstretching = ncread('roms_his.nc','Vstretching');
-theta_s     = ncread('roms_his.nc','theta_s');
-theta_b     = ncread('roms_his.nc','theta_b');
-hc          = ncread('roms_his.nc','hc');
-h           = ncread('roms_his.nc','h');
-%grid        = ncread('roms_his.nc','grid');
+%% Wind check
+
+figure('position', [0, 0, 600, 120])
+plot(time,squeeze(sustr(3,3,:))); datetick('x','dd'); xlim(time([1,end]))
+hold on
+plot(time,squeeze(svstr(3,3,:))); datetick('x','dd'); xlim(time([1,end]))
+
+%% Grid
+
+Vtransform  = ncread(HISname,'Vtransform');
+Vstretching = ncread(HISname,'Vstretching');
+theta_s     = ncread(HISname,'theta_s');
+theta_b     = ncread(HISname,'theta_b');
+hc          = ncread(HISname,'hc');
+h           = ncread(HISname,'h');
+f_Coriolis  = ncread(HISname,'f');
 
 N     = 180;
 igrid = 1; % for RHO points
@@ -57,6 +85,12 @@ ubar_end = ubar(:,:,end);
 vbar_ini = vbar(:,:,1);
 vbar_end = vbar(:,:,end);
 
+ubar_a = squeeze(ubar(3,3,:));
+vbar_a = squeeze(vbar(3,3,:));
+
+% ubar_a = squeeze(u(3,3,end,:));
+% vbar_a = squeeze(v(3,3,end,:));
+
 u_Hov = squeeze(squeeze(u(1,1,:,:)));
 v_Hov = squeeze(squeeze(v(1,1,:,:)));
 temp_Hov = squeeze(squeeze(temp(1,1,:,:)));
@@ -67,7 +101,7 @@ AKv_Hov = squeeze(squeeze(AKv(1,1,:,:)));
 tke_Hov = squeeze(squeeze(tke(1,1,:,:)));
 gls_Hov = squeeze(squeeze(gls(1,1,:,:)));
 
-%%
+%% T-profile
 
 figure('position', [0, 0, 500, 600]);
 plot(squeeze(temp_ini(1,1,:)),squeeze(z_rho(1,1,:)))
@@ -81,7 +115,7 @@ ylim([-300 0])
 % set(gca,'LooseInset', get(gca,'TightInset')); % no blank edge
 % saveas(gcf, [proj_root,'/Figs/tprof'], 'png');
 
-%%
+%% S-profile
 
 figure('position', [0, 0, 500, 600]);
 plot(squeeze(salt_ini(1,1,:)),squeeze(z_rho(1,1,:)))
@@ -94,3 +128,25 @@ ylim([-300 0])
 
 % set(gca,'LooseInset', get(gca,'TightInset')); % no blank edge
 % saveas(gcf, [proj_root,'/Figs/sprof'], 'png');
+
+%% Velocity
+
+cur_a = complex(ubar_a,vbar_a);
+
+t_Coriolis = 2*pi/f_Coriolis(1)/3600; % [hour]
+
+%------- Welch's power spectral density estimate --------------------------
+
+cur_a = cur_a - mean(cur_a);
+ 
+% the next power of 2 greater than signal length - FFT transfrom length
+n = 2^nextpow2(length(cur_a));
+
+% the number of samples per unit time [day]
+fs = 24*3600/(nHIS*dt); 
+
+[p_cur, f] = pwelch(cur_a,[],[],n,fs); % returned f in cycle/day
+
+rotary_spec(f,p_cur,24/t_Coriolis,0)
+%--------------------------------------------------------------------------
+
